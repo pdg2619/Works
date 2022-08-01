298a7ed6d55fc80dd5398f88e0bd070a6e5497c7
Eukarya <50507332+pdg2619@users.noreply.github.com>
2022-08-02 05:27:55 +0900

    Delete 1-1 directory

--- 1-1/1_hello.c
+++
@@ -1,8 +0,0 @@
-﻿/*　Hello, world!を表示するプログラム */
-#include <stdio.h> /* prinf関数を利用するためにインクルードする．*/
-
-int main(void)  /* main関数を開始する．*/
-{
-    printf("Hello, world!\n"); /*　Hello, world!を表示する */
-    return (0); /* 0 を戻してmain関数を終了する. */
-}
--- 1-1/2_num_name.c
+++
@@ -1,8 +0,0 @@
-﻿/*　`学生番号と名前を表示するプログラム */
-#include <stdio.h>    /* printf関数を利用するためにインクルードする. */
-
-int main(void)    /* main関数を開始する. */
-{
-    printf("S173102 朴 東均(PARK DONGKYUN), \n");    /* 学生番号, 氏名を表示する. */
-    return (0);    /* 0を戻してmain関数を終了する. */
-}
