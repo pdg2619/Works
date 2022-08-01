83498e6fc2661381ebe79bc39b9669863d1a6aec
Eukarya <50507332+pdg2619@users.noreply.github.com>
2022-08-02 05:34:40 +0900

    Add files via upload

---
+++ C_program_201804-201807/Report3/ou-6-1.c
@@ -0,0 +1,30 @@
+﻿/*  Fibonacci数列を計算するプログラム. */
+#include <stdio.h>
+
+#define MAX_NUM_FIBONACCI 60		/* 定数マクロ MAX_NUM_FIBONACCIを 43 で定義する．*/
+
+double Fibonacci_compute(int n) 	/* Fibonacci数列を計算する関数. */
+{
+	if( n < 2 ){
+	return(1);
+	}
+	else {
+	return(Fibonacci_compute(n-1) + Fibonacci_compute(n-2));
+	}
+}
+int main(void)				/* main関数を開始する. */
+{
+ 	int i;			/*int 型の変数 i 宣言 */		
+	
+	for(i=1; i<=MAX_NUM_FIBONACCI; i++) 	/* 変数iが(i = MAX_NUM_FIBONACCI)を満たすまでに、以下の文を繰り返す．*/
+	{  
+		if( i < MAX_NUM_FIBONACCI){	/* 数列の途中には「，」を付けるし, 数列の末端は空白にする条件文*/
+		printf("%14.0lf, ", Fibonacci_compute(i)); 
+		}
+		else{
+		printf("%14.0lf", Fibonacci_compute(i));
+		}		 
+	}  
+	printf("\n"); 
+	return(0); 
+}
---
+++ C_program_201804-201807/Report3/ou-6-4.c
@@ -0,0 +1,38 @@
+﻿/*  Collatz予想を計算するプログラム. */
+#include <stdio.h>
+
+int Collatz_seq_compute (int x)	/*  Collatz予想を計算する関数. */
+{
+		if (x % 2 == 0) {
+                	x = (x / 2);
+			return(x);
+		} 
+		else {
+                x = ((x * 3) + 1);
+			return(x);
+		}
+ }
+
+
+int main(void)			/* main関数を開始する. */
+{
+ 	int n, k;		/* int 型の変数 n, k 宣言. */
+	int l = 0;		/* int 型の変数 n, k 宣言し、初期化. */
+		
+	scanf ("%d", &n);	/* キーボードで整数nを入力. */
+	k = n;			/* kにnを代入. (kはnの最初の値を保存する変数)　*/
+	
+	if ( n >= 1 && n <= 80){ /* nの値が　1≤n≤80 を満たすとき、以下の演算を進行. */
+			while(n > 1){ /* nの値が　1 < n を満たすと、以下の演算を反復進行. */
+				n = Collatz_seq_compute (n);	/* nにCollatz予想関数で計算した値を代入. */
+				printf(" → ");	/*「 → 」を表示. */	
+				printf("%d",n);	/*「 n 」を表示. */
+				l++;		/* lの値を1増加. (lはCollatz予想の長さ.) */
+				continue;	/* while文を続く. */
+		}
+	}
+	printf("\n"); 
+	printf("index = %d, max length = %d", k, l);	/* index(k) と max length(l)を表示. */
+	printf("\n"); 
+	return(0); 
+}
