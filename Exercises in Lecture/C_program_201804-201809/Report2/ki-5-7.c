aa91e28c883f819dbe9713a1884721af6f19ac80
Eukarya <50507332+pdg2619@users.noreply.github.com>
2022-08-06 00:09:14 +0900

    Add files via upload

---
+++ Exercises in Lecture/C_program_201804-201809/Report2/ki-5-7.c
@@ -0,0 +1,45 @@
+﻿/* 行列の自乗を求めて表示するプログラミング */
+#include <stdio.h>
+
+#define NUM_MATRIX_ROW		2	/* マクロで, 行列の行数を(NUM_MATRIX_ROW)を指定. */ 
+#define NUM_MATRIX_COLUMN	2	/* マクロで, 行列の列数を(NUM_MATRIX_COLUMN)を指定. */ 
+
+
+int main(void)
+{
+	/* int 型の変数 i, j , sum1, sum2, k を宣言する.  */
+	int i, j, sum1, sum2, k;					
+	int a[NUM_MATRIX_ROW][NUM_MATRIX_COLUMN] = {{0, 0}, {3, 0}};	/* 行列Aの成分を指定.  */
+	int b[NUM_MATRIX_ROW][NUM_MATRIX_COLUMN] = {{1, 1}, {-1, -1}};	/* 行列Bの成分を指定.  */
+	int x1[NUM_MATRIX_ROW][NUM_MATRIX_COLUMN] = {{0}};		/* 行列A²の成分を初期化. */
+	int x2[NUM_MATRIX_ROW][NUM_MATRIX_COLUMN] = {{0}};		/* 行列B²の成分を初期化. */
+	
+	for ( i = 0; i < NUM_MATRIX_ROW; i++ ) {			/* 行列A², B²を求める.  */
+		for (j = 0; j < NUM_MATRIX_COLUMN; j++) {
+			sum1 = 0;
+			sum2 = 0;	
+			for (k=0; k<2; k++) {
+				sum1 += a[i][k] * a[k][i];		/* 行列A²の各成分を求める. */	
+				sum2 += b[i][k] * b[k][i];		/* 行列B²の各成分を求める. */
+			}
+			x1[i][j] = sum1;				/* 行列A²の各成分を配列に代入. */
+			x2[i][j] = sum2;				/* 行列B²の各成分を配列に代入. */
+		}
+	}
+	printf("A²\n");							/* 行列A²を表示.  */
+	for ( i = 0; i < NUM_MATRIX_ROW; i++ ) {		
+		for (j = 0; j < NUM_MATRIX_COLUMN; j++) {
+		printf("%2d  ", x1[i][j]);
+			}
+	printf("\n");
+	}
+	printf("B²\n");							/* 行列B²を表示.  */
+	for ( i = 0; i < NUM_MATRIX_ROW; i++ ) {		
+		for (j = 0; j < NUM_MATRIX_COLUMN; j++) {
+		printf("%2d  ", x2[i][j]);
+			}
+	printf("\n");
+	}
+	
+	return (0);							/* 0 を戻してmain関数を終了する. */
+}
---
+++ Exercises in Lecture/C_program_201804-201809/Report2/ou-5-1.c
@@ -0,0 +1,38 @@
+﻿/* 数列の最大値, 最小値と算術·幾何平均を求め表示するプログラミング */
+#include <stdio.h>
+#include <math.h>
+
+#define NUM_ARRAY	5
+
+
+int main(void)
+{
+	int i, max = 0, min = 0;			/* int 型の変数 i, max, min を宣言する.  */
+	int a[NUM_ARRAY] = {83, 95, 85 ,63, 89};	/* 配列を [NUM_ARRAY]項まで宣言し, 数列を代入. */
+	
+	/*「数列の最大値, 最小値と算術·幾何平均を求めますす。」を表示. */
+	printf("数列の最大値, 最小値と算術·幾何平均を求めます。\n");
+
+	min = max = a[0];				/* 変数 min, maxを第0項に設定. */
+
+	for ( i = 0; i < NUM_ARRAY; i++ ) {		/* 数列の数を比較して最小値と最大値を求める. */
+		if ( i != ( NUM_ARRAY - 1 ) ) {
+			printf("a[%d] = %d, ", i, a[i]);
+		}
+		else {
+			printf("a[%d] = %d\n", i, a[i]);
+		}
+
+		if ( ( i > 0 ) && ( a[i] > max ) ) {
+			max = a[i];
+		}
+
+		if ( ( i > 0 ) && ( a[i] < min ) ) {
+			min = a[i];
+		}
+	}
+	/* 変数 min, maxを表示し, 算術·幾何平均を求め表示. */
+	printf("max = %d, min = %d\narithmetic mean = %lf , geometrical mean = %lf\n", max, min, ((double)(max+min)/2), ((double)sqrt((max)*(min))));
+		
+	return (0);
+}
